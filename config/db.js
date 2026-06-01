f3c0a964aa690fda596d03e0ac82ff08a3ee8ae8
Vaibhavi Suradkar <vaibhavisuradkar105@gmail.com>
2026-06-01 19:50:46 +0530

    feat: added DB connection validation

--- config/db.js
+++ config/db.js
@@ -2,6 +2,11 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
+  //Added check to ensure MONGO_URI is defined in environment variables
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is not defined in environment variables");
+  }
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connected successfully");
